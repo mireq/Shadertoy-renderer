f442b82ee712bac35800d29c43182a29ed07c27f
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-07-19 05:19:35 +0200

    Minimal code to render shader

--- shadertoy.py
+++ shadertoy.py
@@ -57,6 +57,7 @@ class RenderPass(object):
 		self.type = pass_definition['type']
 		if len(self.outputs) != 1:
 			raise NotImplementedError("Pass with %d outputs not implemented" % len(outputs))
+		self.shader = self.make_shader()
 
 	@staticmethod
 	def create(renderer, pass_definition):
@@ -65,20 +66,70 @@ class RenderPass(object):
 		else:
 			raise NotImplementedError("Shader pass %s not implemented" % pass_definition['type'])
 
+	def make_shader(self):
+		shader = make_shader({'vertex': self.vertex_shader_code, 'fragment': self.get_fragment_shader()})
+		gl.glUseProgram(shader['program'])
+
+		stride = self.renderer.vertex_surface.strides[0]
+		offset = ctypes.c_void_p(0)
+		loc = gl.glGetAttribLocation(shader['program'], "position")
+		gl.glEnableVertexAttribArray(loc)
+		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.renderer.vertex_surface_buffer)
+		gl.glVertexAttribPointer(loc, 2, gl.GL_FLOAT, False, stride, offset)
+
+		loc = gl.glGetUniformLocation(shader['program'], "iResolution")
+		gl.glUniform3f(loc, float(self.renderer.resolution[0]), float(self.renderer.resolution[1]), float(0))
+
+		return shader
+
 	def render(self):
 		raise NotImplementedError()
 
+	def get_fragment_shader(self):
+		# samplerCube
+		i_channels = ['sampler2D'] * 4
+		sampler_code = ''.join(f'uniform {sampler} iChannel{num};\n' for num, sampler in enumerate(i_channels))
+
+		return """#version 120
+
+uniform vec3      iResolution;           // viewport resolution (in pixels)
+uniform float     iTime;                 // shader playback time (in seconds)
+uniform float     iTimeDelta;            // render time (in seconds)
+uniform int       iFrame;                // shader playback frame
+uniform float     iChannelTime[4];       // channel playback time (in seconds)
+uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+uniform vec4      iDate;                 // (year, month, day, time in seconds)
+uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
+%s
+
+%s
+
+void main()
+{
+	mainImage(gl_FragColor, gl_FragCoord.xy);
+}
+
+""" % (sampler_code, self.code)
+
+
 
 class ImageRenderPass(RenderPass):
-	vertex_shader_code = """attribute vec2 position;
+	vertex_shader_code = """#version 120
+
+attribute vec2 position;
+varying vec4 texcoord;
 void main()
 {
 	gl_Position = vec4(position, 0.0, 1.0);
+	texcoord = vec4(position, 0.0, 1.0);
 }"""
-	fragment_shader_code = """
+	fragment_shader_code = """#version 120
+varying vec4 texcoord;
+
 void main()
 {
-	gl_FragColor = vec4(1, 1, 1, 1);
+	gl_FragColor = texcoord;
 }"""
 
 	def __init__(self, *args, **kwargs):
@@ -86,43 +137,39 @@ void main()
 		self.framebuffer = gl.glGenFramebuffers(1)
 		self.image = gl.glGenTextures(1)
 		gl.glBindTexture(gl.GL_TEXTURE_2D, self.image)
-		gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, self.renderer.window_size[0], self.renderer.window_size[1], 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, None)
+		gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, self.renderer.resolution[0], self.renderer.resolution[1], 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, None)
 		gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST);
 		gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST);
-		self.shader = make_shader({'vertex': self.vertex_shader_code, 'fragment': self.fragment_shader_code})
-
-		stride = self.renderer.vertex_surface.strides[0]
-		offset = ctypes.c_void_p(0)
-		loc = gl.glGetAttribLocation(self.shader['program'], "position")
-		gl.glEnableVertexAttribArray(loc)
-		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.renderer.vertex_surface_buffer)
-		gl.glVertexAttribPointer(loc, 2, gl.GL_FLOAT, False, stride, offset)
 
 	def render(self):
 		gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.framebuffer);
 		gl.glFramebufferTexture2D(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, self.image, 0);
-		gl.glViewport(0, 0, *self.renderer.window_size)
+		gl.glViewport(0, 0, *self.renderer.resolution)
 		gl.glUseProgram(self.shader['program'])
 		gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
 		gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0);
 
 
 class Renderer(object):
-	vertex_shader_code = """attribute vec2 position;
+	vertex_shader_code = """#version 120
+
+attribute vec2 position;
 varying vec2 texcoord;
 void main()
 {
 	gl_Position = vec4(position, 0.0, 1.0);
 	texcoord = position * vec2(0.5, -0.5) + vec2(0.5);
 }"""
-	fragment_shader_code = """varying vec2 texcoord;
+	fragment_shader_code = """#version 120
+
+varying vec2 texcoord;
 uniform sampler2D texture;
 void main()
 {
 	gl_FragColor = texture2D(texture, texcoord);
 }"""
 
-	def __init__(self, shader_definition, window_size=None):
+	def __init__(self, shader_definition, resolution=None):
 		self.vertex_surface = np.array([(-1,+1), (+1,+1), (-1,-1), (+1,-1)], np.float32)
 		self.vertex_surface_buffer = gl.glGenBuffers(1)
 		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vertex_surface_buffer)
@@ -138,7 +185,7 @@ void main()
 		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vertex_surface_buffer)
 		gl.glVertexAttribPointer(loc, 2, gl.GL_FLOAT, False, stride, offset)
 
-		self.window_size = window_size or (512, 512)
+		self.resolution = resolution or (512, 512)
 		self.render_passes = []
 		self.output = None
 		for render_pass_definition in shader_definition['renderpass']:
@@ -155,6 +202,10 @@ void main()
 		self.frame += 1
 		sys.stdout.write(f'\r{self.frame}')
 		sys.stdout.flush()
+
+		for render_pass in self.render_passes:
+			render_pass.render()
+
 		gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 		gl.glUseProgram(self.shader['program'])
 		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vertex_surface_buffer)
@@ -168,19 +219,31 @@ void main()
 		glut.glutSwapBuffers()
 
 	def reshape(self, width=0, height=0):
-		for render_pass in self.render_passes:
-			render_pass.render()
-		glut.glutReshapeWindow(*self.window_size);
-		gl.glViewport(0, 0, *self.window_size)
+		gl.glViewport(0, 0, width, height)
 
 	def keyboard(self, key, x, y):
 		if key == b'\x1b':
 			sys.exit()
 
 
+def parse_resolution(val):
+	msg = "Resulution in wrong format, expected widthxheight e.g. 1920x1080"
+	try:
+		width, height = val.split('x')
+	except ValueError:
+		raise argparse.ArgumentTypeError(msg)
+	try:
+		width = int(width)
+		height = int(height)
+	except ValueError:
+		raise argparse.ArgumentTypeError(msg)
+	return (width, height)
+
+
 def main():
 	parser = argparse.ArgumentParser(description="Shadertoy renderer")
 	parser.add_argument('file', type=argparse.FileType('r'), help="Shader file")
+	parser.add_argument('--resolution', type=parse_resolution, default=(480, 270), help="Resolution")
 	args = parser.parse_args()
 
 	os.environ['vblank_mode'] = '0'
@@ -189,12 +252,12 @@ def main():
 	glut.glutInitDisplayMode(glut.GLUT_DOUBLE | glut.GLUT_RGBA)
 	glut.glutCreateWindow("Shadertoy")
 
-	renderer = Renderer(json.load(args.file))
+	renderer = Renderer(json.load(args.file), args.resolution)
 	glut.glutDisplayFunc(renderer.display)
 	glut.glutReshapeFunc(renderer.reshape)
 	glut.glutKeyboardFunc(renderer.keyboard)
 	glut.glutIdleFunc(renderer.display)
-	renderer.reshape()
+	glut.glutReshapeWindow(*args.resolution);
 
 	glut.glutMainLoop()
 

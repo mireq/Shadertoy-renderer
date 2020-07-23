08e7cd2996f93474ca59477577c941a1d1088944
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-07-23 05:56:28 +0200

    Added rendering to video file

--- shadertoy.py
+++ shadertoy.py
@@ -2,13 +2,19 @@
 import argparse
 import ctypes
 import json
+import logging
 import os
+import shlex
+import subprocess
 import sys
 import time
 
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
 import numpy as np
+
+
+logger = logging.getLogger(__name__)
 
 
 FRAGMENT_SHADER_TEMPLATE = """#version 130
@@ -64,6 +70,8 @@ void main()
 }
 """
 
+FFMPEG_CMDLINE = '{ffmpeg} -r {framerate} -f rawvideo -s {resolution} -pix_fmt rgb24 -i {input} -vf vflip -an -y -crf 15 -c:v libx264 -pix_fmt yuv420p -preset slow {output}'
+
 
 def compile_shader(shader):
 	gl.glCompileShader(shader)
@@ -100,6 +108,36 @@ def make_shader(source):
 	if 'fragment' in source:
 		gl.glDetachShader(shader['program'], shader['fragment'])
 	return shader
+
+
+class FrameRateControllerResult(object):
+	def __init__(self, emit_frames, merge_frames):
+		self.emit_frames = emit_frames
+		self.merge_frames = merge_frames
+
+	def __repr__(self):
+		return f'emit_frames={self.emit_frames}, merge_frames={self.merge_frames}'
+
+
+class FrameRateController(object):
+	def __init__(self, fps, out_fps):
+		self.frame = 0
+		self.fps = fps
+		self.out_fps = out_fps
+		self.last_frame = 0
+		self.last_output_frame = 0
+
+	def on_frame(self):
+		self.frame += 1
+		output_frame = (self.out_fps * self.frame) // self.fps
+		if output_frame == self.last_output_frame:
+			return FrameRateControllerResult(emit_frames=0, merge_frames=0)
+		else:
+			emit_frames = output_frame - self.last_output_frame
+			merge_frames = self.frame - self.last_frame
+			self.last_output_frame = output_frame
+			self.last_frame = self.frame
+			return FrameRateControllerResult(emit_frames=emit_frames, merge_frames=merge_frames)
 
 
 class RenderPass(object):
@@ -253,13 +291,33 @@ class Renderer(object):
 
 		self.shader_filename = args.file.name
 		self.resolution = args.resolution
+
+		self.image_pack_buffer = gl.glGenBuffers(1)
+		gl.glBindBuffer(gl.GL_PIXEL_PACK_BUFFER, self.image_pack_buffer)
+		gl.glBufferData(gl.GL_PIXEL_PACK_BUFFER, self.resolution[0] * self.resolution[1] * 3, None, gl.GL_STREAM_READ)
+		gl.glBindBuffer(gl.GL_PIXEL_PACK_BUFFER, 0)
+		self.current_image = np.empty((self.resolution[0], self.resolution[1], 3), dtype=np.uint8)
+		self.cumulative_image = np.zeros((self.resolution[0], self.resolution[1], 3), dtype=np.uint32)
+
 		self.tile_size = self.resolution
 		if args.tile_size:
 			self.tile_size = args.tile_size
+		self.fps = args.fps
+		self.render_video = args.render_video
+		self.render_video_fps = args.render_video_fps
+		if args.render_video and self.fps is None:
+			self.fps = 60
+		if args.render_video and self.render_video_fps is None:
+			self.render_video_fps = self.fps
+		self.video_framerate_controller = None
+		if args.render_video:
+			self.video_framerate_controller = FrameRateController(self.fps, self.render_video_fps)
 		self.tiles = self.__calc_tiles()
 		self.multisample = args.multisample
 		self.render_passes = []
 		self.output = None
+		self.ffmpeg = None
+		self.ffmpeg_feed = None
 		self.start_time = time.monotonic()
 		for render_pass_definition in shader_definition['renderpass']:
 			render_pass = RenderPass.create(self, render_pass_definition)
@@ -272,8 +330,20 @@ class Renderer(object):
 		self.frame = 0
 
 	def display(self):
-		self.render_frame()
+		self.render_offscreen()
+		self.render_display()
 
+	def render_offscreen(self):
+		try:
+			gl.glViewport(0, 0, self.resolution[0], self.resolution[1])
+			self.render_frame()
+			self.write_video()
+		except Exception:
+			logger.exception("Exception in display method")
+			sys.exit()
+
+	def render_display(self):
+		gl.glViewport(0, 0, glut.glutGet(glut.GLUT_WINDOW_WIDTH), glut.glutGet(glut.GLUT_WINDOW_HEIGHT))
 		gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 		gl.glUseProgram(self.shader['program'])
 		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vertex_surface_buffer)
@@ -295,18 +365,53 @@ class Renderer(object):
 
 	def keyboard(self, key, x, y):
 		if key == b'\x1b':
+			self.quit()
 			sys.exit()
 
 	def render_frame(self):
-		self.frame += 1
-		sys.stdout.write(f'\r{self.frame}')
-		sys.stdout.flush()
+		inputs = {}
+		if self.fps is None:
+			inputs['time'] = time.monotonic() - self.start_time
+		else:
+			inputs['time'] = self.frame / self.fps
 
 		for render_pass in self.render_passes:
-			render_pass.update_inputs({
-				'time': time.monotonic() - self.start_time,
-			})
+			render_pass.update_inputs(inputs)
 			render_pass.render()
+
+		self.frame += 1
+		#sys.stdout.write("\r%d" % self.frame)
+		#sys.stdout.flush()
+
+	def write_video(self):
+		if not self.render_video:
+			return
+		if self.ffmpeg_feed is None:
+			self.__init_video_output()
+
+		gl.glBindBuffer(gl.GL_PIXEL_PACK_BUFFER, self.image_pack_buffer)
+		gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.output.framebuffer);
+		gl.glReadPixels(0, 0, self.resolution[0], self.resolution[1], gl.GL_RGB, gl.GL_UNSIGNED_BYTE, 0);
+
+		buffer_addr = gl.glMapBuffer(gl.GL_PIXEL_PACK_BUFFER, gl.GL_READ_ONLY)
+		ctypes.memmove(self.current_image.ctypes.data, buffer_addr, self.resolution[0] * self.resolution[1] * 3)
+		gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0);
+		gl.glUnmapBuffer(gl.GL_PIXEL_PACK_BUFFER)
+		gl.glBindBuffer(gl.GL_PIXEL_PACK_BUFFER, 0)
+
+		frame_action = self.video_framerate_controller.on_frame()
+
+		self.cumulative_image += self.current_image
+		if frame_action.emit_frames:
+			self.current_image = (self.cumulative_image / frame_action.merge_frames).astype(np.uint8)
+			for __ in range(frame_action.emit_frames):
+				self.ffmpeg_feed.write(self.current_image.tobytes())
+			self.cumulative_image = np.zeros((self.resolution[0], self.resolution[1], 3), dtype=np.uint32)
+
+	def quit(self):
+		if self.ffmpeg is not None:
+			self.ffmpeg.stdin.close()
+			self.ffmpeg.wait()
 
 	def __calc_tiles(self):
 		tiled_width = (self.resolution[0] + self.tile_size[0] - 1) // self.tile_size[0]
@@ -322,6 +427,21 @@ class Renderer(object):
 				)
 				tiles.append(tile)
 		return tiles
+
+	def __init_video_output(self):
+		self.ffmpeg_feed = False
+		replacements = {
+			'ffmpeg': 'ffmpeg',
+			'resolution': f'{self.resolution[0]}x{self.resolution[1]}',
+			'input': '-',
+			'framerate': str(self.render_video_fps),
+			'output': self.render_video,
+		}
+		replacements = {'{'+key+'}': val for key, val in replacements.items()}
+		params = shlex.split(FFMPEG_CMDLINE)
+		params = [replacements.get(param, param) for param in params]
+		self.ffmpeg = subprocess.Popen(params, stdin=subprocess.PIPE)
+		self.ffmpeg_feed = self.ffmpeg.stdin
 
 
 def parse_resolution(val):
@@ -344,22 +464,36 @@ def main():
 	parser.add_argument('--resolution', type=parse_resolution, default=(480, 270), help="Resolution")
 	parser.add_argument('--tile-size', type=parse_resolution, help="Tile size")
 	parser.add_argument('--multisample', action='store_true', help="Multisampling")
+	parser.add_argument('--fps', type=int, help="Render frame rate")
+	parser.add_argument('--render-video', help="Path to rendered video file")
+	parser.add_argument('--render-video-fps', type=int, help="Video frame rate")
 	args = parser.parse_args()
 
 	os.environ['vblank_mode'] = '0'
 
 	glut.glutInit()
 	glut.glutInitDisplayMode(glut.GLUT_DOUBLE | glut.GLUT_RGBA)
+	glut.glutInitWindowSize(*args.resolution);
 	glut.glutCreateWindow("Shadertoy")
 
 	renderer = Renderer(json.load(args.file), args)
-	glut.glutDisplayFunc(renderer.display)
-	glut.glutReshapeFunc(renderer.reshape)
+	glut.glutDisplayFunc(renderer.render_display)
+	#glut.glutReshapeFunc(renderer.reshape)
 	glut.glutKeyboardFunc(renderer.keyboard)
-	glut.glutIdleFunc(renderer.idle)
+	#glut.glutIdleFunc(renderer.idle)
 	glut.glutReshapeWindow(*args.resolution);
 
-	glut.glutMainLoop()
+	while True:
+		try:
+			glut.glutMainLoopEvent()
+			renderer.render_offscreen()
+			glut.glutMainLoopEvent()
+			glut.glutPostRedisplay()
+		except KeyboardInterrupt:
+			sys.stdout.write("Stopping\n")
+			sys.stdout.flush()
+			renderer.quit()
+			sys.exit()
 
 
 if __name__ == "__main__":
